451a687078a1cb2feb3e3ba1e02c3788b99839ff
takecian <takecian@gmail.com>
2018-11-26 00:15:25 +0900

    Add homebrew script

---
+++ homebrew_install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "ok. run brew upgrade..."
+
+brew upgrade --all
+
+formulas=(
+    git
+    wget
+    curl
+    tree
+    openssl
+    cask
+    peco
+    hub
+    tig
+    markdown
+)
+
+echo "start brew install apps..."
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
+
+casks=(
+    dropbox
+    bettertouchtool
+    google-chrome
+    google-japanese-ime
+    slack
+    iterm2
+    atom
+    docker
+)
+
+echo "start brew cask install apps..."
+for cask in "${casks[@]}"; do
+    brew cask install $cask
+done
+
+brew cleanup
+brew cask cleanup
+
+cat << END
+
+**************************************************
+HOMEBREW INSTALLED! bye.
+**************************************************
+
+END
+
