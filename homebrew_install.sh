edde65ad0bdfbcd1d46a70f50d2dcb433c72501b
takecian <takecian@gmail.com>
2019-01-16 18:14:17 +0900

    Add line

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -71,7 +71,7 @@ mas install 434290957 # motion
 mas install 866773894 # quiver
 mas install 1278508951 # trello
 mas install 1289197285 # mindnode
-mas install 
+mas install 539883307 # line
 
 cat << END
 
