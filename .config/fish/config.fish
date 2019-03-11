736fe00518ba24c0b131f8a6abf36750b46d9799
takeshi.fujiki <takeshi.fujiki@linecorp.com>
2019-03-11 11:23:59 +0900

    Setup rbenv and pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,3 +19,9 @@ set fish_plugins theme peco
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
+
+# rbenv
+rbenv init - | source
+
+# pyenv
+pyenv init - | source
