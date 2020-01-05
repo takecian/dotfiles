e764c4249fde2aa80ca9fa8a1b67294b8d936d6d
Takeshi Fujiki <takecian@gmail.com>
2020-01-05 22:37:19 +0900

    Remove comment

--- dot_install.sh
+++ dot_install.sh
@@ -7,13 +7,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 
 cd $THIS_DIR
 
-
 echo "Start setup..."
-# for f in .??*; do
-#     [ "$f" = ".git" ] && continue
-
-#     ln -snfv ~/dotfiles/"$f" ~/
-# done
 ln -snfv ~/dotfiles/.config/fish/ ~/.config/
 ln -snfv ~/dotfiles/.gitconfig ~/
 ln -snfv ~/dotfiles/.lldbinit ~/
